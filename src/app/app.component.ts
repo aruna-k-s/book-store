95bd7eb2498961a2a2b1633a850ebae95e0b7919
aruna.k.s <aruna.k.s@blr.cordis>
2020-07-16 09:30:30 +0530

    COMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Epub, { EpubCFI } from 'epubjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'book-store';
+  rendition: any;
+  displayed: any;
+  book: any;
+
+  ngOnInit(): void {
+
+    let xyz = new EpubCFI();
+    this.book = Epub("../assets/epubFiles/igp-twss.epub");
+    console.log(this.book,xyz);
+
+    this.rendition = this.book.renderTo(document.getElementById('area'), { width: 600, height: 400 });
+    this.displayed = this.rendition.display();
+  }
+
+  next(){
+    this.rendition.next();
+  }
+  
 }
